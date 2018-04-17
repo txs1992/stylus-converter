3cc9d0211e49d43751ed39359ad116279e6134ae
TaoXuSheng <xusheng.tao@ele.me>
2018-04-17 15:00:11 +0800

    添加 babel 转换 ES6 语法。

--- bin/index.js
+++ bin/index.js
@@ -4,34 +4,36 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 
 var Parser = _interopDefault(require('stylus/lib/parser.js'));
 
-function repeatString (str, num) {
-  return num > 0 ? str.repeat(num) : ''
+function repeatString(str, num) {
+  return num > 0 ? str.repeat(num) : '';
 }
 
-function nodesToJSON (nodes) {
-  return nodes.map(node => node.toJSON())
+function nodesToJSON(nodes) {
+  return nodes.map(function (node) {
+    return node.toJSON();
+  });
 }
 
-function trimFirst (str) {
-  return str.replace(/(^\s*)/g, '')
+function trimFirst(str) {
+  return str.replace(/(^\s*)/g, '');
 }
 
-function tirmFirstLength (str) {
-  return str.length - trimFirst(str).length
+function tirmFirstLength(str) {
+  return str.length - trimFirst(str).length;
 }
 
-function trimLinefeed (str) {
-  return str.replace(/^\n*/, '')
+function trimLinefeed(str) {
+  return str.replace(/^\n*/, '');
 }
 
-function trimFirstLinefeedLength (str) {
-  return tirmFirstLength(trimLinefeed(str))
+function trimFirstLinefeedLength(str) {
+  return tirmFirstLength(trimLinefeed(str));
 }
 
-let oldLineno = 1;
-let oldColumn = 1;
+var oldLineno = 1;
+var oldColumn = 1;
 
-const TYPE_VISITOR_MAP = {
+var TYPE_VISITOR_MAP = {
   Unit: visitUnit,
   RGBA: visitRGBA,
   Call: visitCall,
@@ -45,120 +47,131 @@ const TYPE_VISITOR_MAP = {
   Arguments: visitArguments
 };
 
-function handleLineno (lineno) {
-  return repeatString('\n', lineno - oldLineno)
+function handleLineno(lineno) {
+  return repeatString('\n', lineno - oldLineno);
 }
 
-function handleColumn (column) {
-  return repeatString(' ', column - oldColumn)
+function handleColumn(column) {
+  return repeatString(' ', column - oldColumn);
 }
 
-function handleLinenoAndColumn ({ lineno, column }) {
-  return handleLineno(lineno) + handleColumn(column)
+function handleLinenoAndColumn(_ref) {
+  var lineno = _ref.lineno,
+      column = _ref.column;
+
+  return handleLineno(lineno) + handleColumn(column);
 }
 
-function findNodesType (list, type) {
-  const nodes = nodesToJSON(list);
-  return nodes.find(node => node.__type === type)
+function findNodesType(list, type) {
+  var nodes = nodesToJSON(list);
+  return nodes.find(function (node) {
+    return node.__type === type;
+  });
 }
 
-function visitNode (node) {
-  const handler = TYPE_VISITOR_MAP[node.__type];
-  return handler ? handler(node) : ''
+function visitNode(node) {
+  var handler = TYPE_VISITOR_MAP[node.__type];
+  return handler ? handler(node) : '';
 }
 
 // 处理 nodes
-function visitNodes (list = []) {
-  let text = '';
-  const nodes = nodesToJSON(list);
-  nodes.forEach(node => { text += visitNode(node); });
-  return text
+function visitNodes() {
+  var list = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+  var text = '';
+  var nodes = nodesToJSON(list);
+  nodes.forEach(function (node) {
+    text += visitNode(node);
+  });
+  return text;
 }
 
 // 处理 import；handler import
-function visitImport (node) {
-  const before = handleLineno(node.lineno) + '@import ';
+function visitImport(node) {
+  var before = handleLineno(node.lineno) + '@import ';
   oldLineno = node.lineno;
-  let quote = '';
-  let text = '';
-  const nodes = nodesToJSON(node.path.nodes || []);
-  nodes.forEach(node => {
+  var quote = '';
+  var text = '';
+  var nodes = nodesToJSON(node.path.nodes || []);
+  nodes.forEach(function (node) {
     text += node.val;
     if (!quote && node.quote) quote = node.quote;
   });
-  return `${before}${quote}${text}${quote};`
+  return '' + before + quote + text + quote + ';';
 }
 
-function visitSelector (node) {
-  let text = handleLinenoAndColumn(node);
+function visitSelector(node) {
+  var text = handleLinenoAndColumn(node);
   oldLineno = node.lineno;
-  return text + visitNodes(node.segments)
+  return text + visitNodes(node.segments);
 }
 
-function visitGroup (node) {
-  const selector = visitNodes(node.nodes);
-  const blockEnd = findNodesType(node.nodes, 'Selector') && selector || '';
-  const block = visitBlock(node.block, blockEnd);
-  return selector + block
+function visitGroup(node) {
+  var selector = visitNodes(node.nodes);
+  var blockEnd = findNodesType(node.nodes, 'Selector') && selector || '';
+  var block = visitBlock(node.block, blockEnd);
+  return selector + block;
 }
 
-function visitBlock (node, selector) {
-  const before = ' {';
-  const after = selector && `\n${handleColumn(trimFirstLinefeedLength(selector) + 1)}}`;
-  let text = '';
+function visitBlock(node, selector) {
+  var before = ' {';
+  var after = selector && '\n' + handleColumn(trimFirstLinefeedLength(selector) + 1) + '}';
+  var text = '';
   text += visitNodes(node.nodes);
-  return `${before}${text}${after}`
+  return '' + before + text + after;
 }
 
-function visitLiteral (node) {
-  return node.val || ''
+function visitLiteral(node) {
+  return node.val || '';
 }
 
-function visitProperty (node) {
-  let text = handleLinenoAndColumn(node);
+function visitProperty(node) {
+  var text = handleLinenoAndColumn(node);
   oldLineno = node.lineno;
-  return `${text + visitNodes(node.segments)}: ${visitExpression(node.expr)};`
+  return text + visitNodes(node.segments) + ': ' + visitExpression(node.expr) + ';';
 }
 
-function visitIdent (node) {
-  return node.name
+function visitIdent(node) {
+  return node.name;
 }
 
-function visitExpression (node) {
-  return visitNodes(node.nodes)
+function visitExpression(node) {
+  return visitNodes(node.nodes);
 }
 
-function visitCall (node) {
-  return `${node.name}(${visitArguments(node.args)})`
+function visitCall(node) {
+  return node.name + '(' + visitArguments(node.args) + ')';
 }
 
-function visitArguments (node) {
-  const nodes = nodesToJSON(node.nodes);
-  let text = '';
-  nodes.forEach((node, idx) => {
-    const prefix = idx ? ', ' : '';
+function visitArguments(node) {
+  var nodes = nodesToJSON(node.nodes);
+  var text = '';
+  nodes.forEach(function (node, idx) {
+    var prefix = idx ? ', ' : '';
     text += prefix + visitNode(node);
   });
-  return text
+  return text;
 }
 
-function visitRGBA (node, prop) {
-  return node.raw
+function visitRGBA(node, prop) {
+  return node.raw;
 }
 
-function visitUnit (node) {
-  return node.val + node.type
+function visitUnit(node) {
+  return node.val + node.type;
 }
 
 // 处理 stylus 语法树；handle stylus Syntax Tree
-function visitor (ast, option) {
-  return visitNodes(ast.nodes) || ''
+function visitor(ast, option) {
+  return visitNodes(ast.nodes) || '';
 }
 
-function converter (result, option = 'scss') {
-  if (typeof result !== 'string') return result
-  const ast = new Parser(result).parse();
-  return visitor(ast, option)
+function converter(result) {
+  var option = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'scss';
+
+  if (typeof result !== 'string') return result;
+  var ast = new Parser(result).parse();
+  return visitor(ast, option);
 }
 
 module.exports = converter;
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
+import babel from 'rollup-plugin-babel'
+
 export default {
   input: './src/index.js',
   output: {
     file: './bin/index.js',
     format: 'cjs'
-  }
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    })
+  ]
 }
