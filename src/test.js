d1cb28cefed3f3a36565d281c66a3996e3683000
TaoXuSheng <xusheng.tao@ele.me>
2018-04-20 14:37:58 +0800

    添加 stylus 测试源文件，修改处理文件代码。

--- src/test.js
+++ src/test.js
@@ -6,10 +6,10 @@ function getPath (address) {
   return path.resolve(__dirname, address)
 }
 
-fs.readFile(getPath('../test/stylus-files/function.styl'), (err, res) => {
+fs.readFile(getPath('../test/test.styl'), (err, res) => {
   if (err) return
   const result = res.toString()
   const ast = new Parser(res.toString()).parse()
   const text = JSON.stringify(ast)
-  fs.writeFile(getPath('../output/function-ast.json'), text, () => {})
+  fs.writeFile(getPath('../test-stylus.json'), text, () => {})
 })
