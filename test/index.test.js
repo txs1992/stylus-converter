604c8046d0813f9daa03af96b7b05e9ff2b83b41
TaoXuSheng <xusheng.tao@ele.me>
2018-04-26 21:30:59 +0800

    修改对于 CSS Selector 的测试

--- test/index.test.js
+++ test/index.test.js
@@ -7,8 +7,8 @@ function getPath (address) {
   return path.resolve(__dirname, address)
 }
 
-describe('测试 CSS 选择器', () => {
-  it('css selector', () => {
+describe('测试 stylus 转换器', () => {
+  it('测试 CSS Selector', () => {
     fs.readFile(getPath('./stylus/selector.styl'), (err, res) => {
       if (err) return
       const result = res.toString()
