36339fa8c7670ba76d252ddd6653bbe06507b7de
MT <xusheng.tao@ele.me>
2018-07-03 12:10:03 +0800

    Update convertVueFile.js
    remove Extra blank line, Add default value.

--- bin/convertVueFile.js
+++ bin/convertVueFile.js
@@ -4,8 +4,8 @@ function convertVueFile(vueTemplate, options) {
   const styleRegEx = /<style(.*)>((\n|.)*)<\/style>/
   const matches = vueTemplate.match(styleRegEx)
   if (Array.isArray(matches) && matches.length >= 2) {
-    if(matches[1].includes('stylus')) {
-      let style = matches[2]
+    if (matches[1].includes('stylus')) {
+      let style = matches[2] || ''
       if (style.trim()) {
         style = converter(style, options)
       }
@@ -16,6 +16,5 @@ function convertVueFile(vueTemplate, options) {
   }
   return vueTemplate;
 }
-
 
 module.exports = convertVueFile;
