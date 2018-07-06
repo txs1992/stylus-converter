0b0e75974708d934a0c7fcf58b596e176d42b658
Christian Memije <christianmemije@gmail.com>
2018-07-06 12:51:02 -0700

    Support vue files with multiple style blocks

---
+++ test/vue/stylus/multiple-style-blocks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="stylus" scoped>
+div
+  color: red
+</style>
+
+<style lang="stylus">
+div
+  color: white
+</style>
+
+<style lang="scss">
+div {
+  color: green;
+}
+</style>
